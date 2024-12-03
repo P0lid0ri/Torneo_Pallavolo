deab06b02cbaf5d9cc83fc6f7f69b17450756561
P0lid0ri <gabrielescuola9910@gmail.com>
2024-12-03 13:30:51 +0100

    aggiunta

---
+++ src/main/java/com/mycompany/torneo_pallavolo/Torneo_pallavolo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.torneo_pallavolo;
+
+/**
+ *
+ * @author GABRIELE
+ */
+public class Torneo_pallavolo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
